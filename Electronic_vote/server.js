1a2539a7551dc8614e7028b8a9a792338f35d89b
BOOMZ <lokiortion@gmail.com>
2018-05-11 22:37:14 +0700

    get/post => createvote and vote 11-05-18 22.36

--- Electronic_vote/server.js
+++ Electronic_vote/server.js
@@ -1,17 +1,33 @@
-const express    =    require('express');
-const bodyParser =    require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 const session = require('express-session')
 const admin = require('firebase-admin');
 const jwt = require('jsonwebtoken');
+const fs = require('fs');
+const Web3 = require('web3');
+const solc = require('solc');
+const util = require('util');
+const truffleContract = require('truffle-contract');
+
+var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8045'));
+var ballotArtifact = JSON.parse(fs.readFileSync('./build/contracts/Ballot.json', 'UTF-8'));
+var Ballot = truffleContract(ballotArtifact);
+Ballot.setProvider(web3.currentProvider);
+
+let vin = '1234567890';
+let cost = 1000000000000000000;
+let gas = 1000000;
+let abi;
+
 
 var serviceAccount = require('./blockchain-1-firebase-adminsdk-477z7-286990727e.json');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://blockchain-1.firebaseio.com'
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://blockchain-1.firebaseio.com'
 });
 
-const app        =    express();
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -20,11 +36,11 @@ app.use(session({
     resave: false,
     saveUninitialized: true
 }))
-app.use('/Style' ,express.static(__dirname + '/views/Style'));
-app.use(function(req, res, next) {
+app.use('/Style', express.static(__dirname + '/views/Style'));
+app.use(function (req, res, next) {
     res.locals.user = req.session.user;
     next();
-  });
+});
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
@@ -32,15 +48,29 @@ app.set('views', './views');
 const isAuthenticated = (req, res, next) => {
     const user = req.session.user;
     console.log(user)
-    if (user !== null && user !== undefined ) {
+    if (user !== null && user !== undefined) {
         next();
     } else {
         res.redirect('/login');
     }
 }
 
-app.get('/', (req, res) => res.render('TestcreateVote.ejs'))
+app.get('/', (req, res) => res.render('index.ejs'))
 app.get('/login', (req, res) => res.render('login.ejs'))
+app.get('/createVote', (req, res) => {
+    let source = fs.readFileSync('./contracts/ballot.sol', 'UTF-8');
+    let compiled = solc.compile(source);
+
+    bin = compiled.contracts[':Ballot'].bytecode;
+
+    // util.log(`>>>>> setup - Bytecode: ${bin}`);
+    // util.log(`>>>>> setup - ABI: ${compiled.contracts[':Ballot'].interface}`);
+
+    abi = JSON.parse(compiled.contracts[':Ballot'].interface);
+
+    util.log('>>>>> setup - Completed !!!')
+    res.render('testCreateVote.ejs')
+})
 app.get('/logout', isAuthenticated, (req, res) => {
     req.session.idToken = null;
     req.session.user = null;
@@ -52,7 +82,7 @@ app.post('/vote', (req, res) => res.render('vote.ejs'))
 
 app.post('/sessionLogin', (req, res) => {
     const toUser = idToken => {
-        const payload = jwt.decode(idToken, {complete: true}).payload;
+        const payload = jwt.decode(idToken, { complete: true }).payload;
         return {
             userId: payload.user_id,
             email: payload.email
@@ -61,15 +91,58 @@ app.post('/sessionLogin', (req, res) => {
     const idToken = req.body.idToken.toString();
     req.session.idToken = idToken;
     req.session.user = toUser(idToken);
-    res.json({status: 'success'})
+    res.json({ status: 'success' })
 });
 
-app.post('/TestcreateVote', (req,res) => {
-    var candidate = req.body.nameCandidate;
-    console.log(candidate);
-    res.json(candidate)
-})
+// Get Voters
+app.get ('/voted', (req,res) => {
+    Ballot.deployed().then(function (instance) {
+        winName = instance.winnerName.call();
+    winName.then(function(resultName){
+        res.json(resultName);
+        console.log(resultName.length);
+    })
+    })
+});
 
+app.post('/TestcreateVote', (req, res) => {
+    var candidate = req.body.nameCandidate;
+    var createCandi;
+    var winName;
+    var blockNum;
+    var BallotContract;
+    var voting;
+    console.log(candidate.length);
+
+    // console.log(ballotArtifact);
+    Ballot.deployed().then(function (instance) {
+        createCandi = instance.Ballot_box.sendTransaction(candidate, 1575158400, { from: web3.eth.coinbase, gas: 6721975 });
+        voting = instance.vote.sendTransaction(1, {from: web3.eth.coinbase});
+        winName = instance.winnerName.call();
+        createCandi.then(function(result){
+            console.log(result);
+        })
+        voting.then(function(voteScore){ 
+            console.log(voteScore);
+        })
+        // winName.then(function(resultName){
+        //     console.log(resultName);
+        // })
+        // })
+        // blockNum = (web3.eth.getBlock("latest").number)+1;
+        // console.log(blockNum);
+        // contractAdd = web3.eth.getBlock("latest");
+        // console.log(contractAdd);
+        // console.log(create.winnerName());
+        // instance.Ballot.call().then(function(result){
+        // console.log(result);
+        // console.log(BallotContract)
+        // console.log(Ballot);
+    }).catch(function (err) {
+        console.log(err);
+    })
+    res.json(candidate);
+});
 app.get('/auth', isAuthenticated, (req, res) => {
     res.json(req.session.user)
 });
--- Electronic_vote/views/Style/js/createvote.js
+++ Electronic_vote/views/Style/js/createvote.js
@@ -2,14 +2,14 @@ if (typeof web3 !== 'undefined') {
     web3 = new Web3(web3.currentProvider);
 } else {
     // set the provider you want from Web3.providers
-    web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
+    web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8045"));
 }
 
 web3.eth.defaultAccount = web3.eth.accounts[0];
 
 var BallotContract = web3.eth.contract([ { "constant": false, "inputs": [ { "name": "proposal", "type": "uint256" } ], "name": "vote", "outputs": [], "payable": true, "stateMutability": "payable", "type": "function" }, { "constant": true, "inputs": [ { "name": "", "type": "uint256" } ], "name": "proposals", "outputs": [ { "name": "name", "type": "bytes32" }, { "name": "voteCount", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [ { "name": "proposalNames", "type": "bytes32[]" }, { "name": "_timeToExpiry", "type": "uint256" } ], "name": "Ballotbox", "outputs": [], "payable": true, "stateMutability": "payable", "type": "function" }, { "constant": true, "inputs": [], "name": "chairperson", "outputs": [ { "name": "", "type": "address" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "timeToExpiry", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "winningProposal", "outputs": [ { "name": "winningProposal_", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "startTime", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [ { "name": "voter", "type": "address" } ], "name": "giveRightToVote", "outputs": [], "payable": true, "stateMutability": "payable", "type": "function" }, { "constant": true, "inputs": [ { "name": "", "type": "address" } ], "name": "voters", "outputs": [ { "name": "weight", "type": "uint256" }, { "name": "voted", "type": "bool" }, { "name": "delegate", "type": "address" }, { "name": "vote", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "getNow", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "timeNow", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "winnerName", "outputs": [ { "name": "", "type": "string" } ], "payable": false, "stateMutability": "view", "type": "function" } ]);
 
-var Ballot = BallotContract.at('0x0d5834723d3d98096a692776115e16248d92f35f');
+var Ballot = BallotContract.at('0x727193b324b2ac6242e5806b89be8b04a7ee83c8');
         console.log(Ballot);
 
 $(document).ready(function() {
